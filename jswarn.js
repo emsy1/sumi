03a135c49131d3c7959244c7682b5f5d5e973231
emsy <67200786+emsy1@users.noreply.github.com>
2021-07-29 11:50:57 +0200

    added javascript warning banner

---
+++ jswarn.js
@@ -0,0 +1,7 @@
+//(c) emsy 2021 - emsy.xyz
+window.onload = jswarn;
+
+function jswarn() {
+  var js_banner_rem = document.getElementById('jswarn');
+               js_banner_rem.remove();
+           }
